b77f256fcbf5418335704cce89f6e71a6f38b073
Daniel Hall <daniel.hall@live.se>
2019-08-26 20:07:37 +0000

    Added more tests

--- test/LinearAllocatorTests.cpp
+++ test/LinearAllocatorTests.cpp
@@ -9,9 +9,29 @@ struct FourByteStruct {
   char Four;
 };
 
-struct FiveByteStruct {
+struct FiveByteStructWithPadding {
   char One;
   int32_t Two;
+};
+
+class FourByteClass {
+ public:
+  FourByteClass()
+      : m_One(0), m_Two(0), m_Three(0), m_CopyConstructorInvoked(false) {}
+
+  ~FourByteClass() = default;
+
+  FourByteClass(const FourByteClass& rhs) {
+    m_One = rhs.m_One;
+    m_Two = rhs.m_Two;
+    m_Three = rhs.m_Three;
+    m_CopyConstructorInvoked = true;
+  }
+
+  char m_One;
+  char m_Two;
+  char m_Three;
+  bool m_CopyConstructorInvoked;
 };
 
 class LinearAllocatorTest : public ::testing::Test {
@@ -91,8 +111,6 @@ TEST_F(
   Allocator.Clear();
 }
 
-// TODO: three char struct
-
 TEST_F(
     LinearAllocatorTest,
     allocating_a_four_byte_aligned_object_after_an_unaligned_allocation_adjusts_for_alignment) {
@@ -122,17 +140,17 @@ TEST_F(
     allocating_a_five_byte_struct_yields_correct_allocation_for_a_four_byte_aligned_allocator) {
   Allocators::LinearAllocator Allocator(BlobSize, Memory);
 
-  FiveByteStruct* AllocatedPtr =
-      Allocators::AllocateNew<FiveByteStruct>(Allocator);
+  FiveByteStructWithPadding* AllocatedPtr =
+      Allocators::AllocateNew<FiveByteStructWithPadding>(Allocator);
 
-  ASSERT_EQ(sizeof(FiveByteStruct), 8);
+  ASSERT_EQ(sizeof(FiveByteStructWithPadding), 8);
   ASSERT_EQ(Allocator.GetSize(), BlobSize);
   ASSERT_TRUE(Allocator.GetStart() != nullptr);
   ASSERT_EQ(Allocator.GetNumberOfAllocations(), 1);
   ASSERT_EQ(Allocator.GetUsedMemory(), 8);
 
-  FiveByteStruct* SecondAllocatedPtr =
-      Allocators::AllocateNew<FiveByteStruct>(Allocator);
+  FiveByteStructWithPadding* SecondAllocatedPtr =
+      Allocators::AllocateNew<FiveByteStructWithPadding>(Allocator);
 
   ASSERT_EQ(Allocator.GetNumberOfAllocations(), 2);
   ASSERT_EQ(Allocator.GetUsedMemory(), 16);
@@ -148,21 +166,19 @@ TEST_F(LinearAllocatorTest, allocation_returns_nullptr_when_overflow_occur) {
   const uint32_t Size = 2;
   Allocators::LinearAllocator Allocator(Size, Memory);
 
-  FiveByteStruct* AllocatedPtr =
-      Allocators::AllocateNew<FiveByteStruct>(Allocator);
+  FiveByteStructWithPadding* AllocatedPtr =
+      Allocators::AllocateNew<FiveByteStructWithPadding>(Allocator);
 
   ASSERT_EQ(AllocatedPtr, nullptr);
 }
 
-// TODO: clear resets value
-
 TEST_F(LinearAllocatorTest, clear_resets_the_allocator) {
   Allocators::LinearAllocator Allocator(BlobSize, Memory);
 
-  FiveByteStruct* AllocatedPtr =
-      Allocators::AllocateNew<FiveByteStruct>(Allocator);
+  FiveByteStructWithPadding* AllocatedPtr =
+      Allocators::AllocateNew<FiveByteStructWithPadding>(Allocator);
 
-  ASSERT_EQ(sizeof(FiveByteStruct), 8);
+  ASSERT_EQ(sizeof(FiveByteStructWithPadding), 8);
   ASSERT_EQ(Allocator.GetSize(), BlobSize);
   ASSERT_TRUE(Allocator.GetStart() != nullptr);
   ASSERT_EQ(Allocator.GetNumberOfAllocations(), 1);
@@ -190,7 +206,39 @@ TEST_F(LinearAllocatorTest, deallocating_leads_to_an_assert) {
 }
 #endif  // DEBUG
 
+TEST_F(LinearAllocatorTest,
+       allocator_taking_an_instance_invokes_copy_constructor) {
+  Allocators::LinearAllocator Allocator(BlobSize, Memory);
+
+  FourByteClass AllocatedClass;
+  AllocatedClass.m_One = 1;
+  AllocatedClass.m_Two = 2;
+  AllocatedClass.m_Three = 3;
+
+  FourByteClass* AllocatedPtr =
+      Allocators::AllocateNew<FourByteClass>(Allocator, AllocatedClass);
+
+  ASSERT_EQ(AllocatedPtr->m_One, 1);
+  ASSERT_EQ(AllocatedPtr->m_Two, 2);
+  ASSERT_EQ(AllocatedPtr->m_Three, 3);
+  ASSERT_TRUE(AllocatedPtr->m_CopyConstructorInvoked);
+
+  ASSERT_EQ(Allocator.GetSize(), BlobSize);
+  ASSERT_TRUE(Allocator.GetStart() != nullptr);
+  ASSERT_EQ(Allocator.GetNumberOfAllocations(), 1);
+  ASSERT_EQ(Allocator.GetUsedMemory(), 4);
+
+  Allocator.Clear();
+}
+
+// TODO: Add test for allocator using variadric template
+
+// TODO: Add test for allocator taking an instance of a object (should invoke
+// the copy constructor)
+
 // TODO: Array allocation correctly with different alignments
+
+// TODO: Add test for allocator deleteing array correctly
 
 // TODO: Validate constructor is invoked for single allocation
 
